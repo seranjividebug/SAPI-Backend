2a2f662f7c9defb073eabd5c3482c5bec7b401d7
Malini <Siranjeevi9597536779@gmail.com>
2026-04-06 22:01:18 +0530

    fix

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ fastify.addContentTypeParser('text/plain', { parseAs: 'string' }, function (req,
 });
 
 fastify.register(postgres, {
-  connectionString: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+  connectionString: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?search_path=sapi,public`,
   ssl: {
     rejectUnauthorized: false
   }
